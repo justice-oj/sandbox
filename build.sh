9e3573c218a37ce1654d521c1cb93a45d4772241
liuchao21 <liuchao21@58ganji.com>
2017-09-01 17:51:56 +0800

    update compiler path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm bin/*
-go build -o bin/c_compiler src/c/compiler.go
-go build -o bin/cpp_compiler src/cpp/compiler.go
+go build -o bin/c_compiler src/sandbox/c/compiler.go
+go build -o bin/cpp_compiler src/sandbox/cpp/compiler.go
