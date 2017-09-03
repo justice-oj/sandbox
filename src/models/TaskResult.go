bbf2f4b11070f4378509020ef878a70b2d320130
liupangzi <thesedays@126.com>
2017-09-03 12:54:13 +0800

    1. add constructors to TaskResult

--- src/models/TaskResult.go
+++ src/models/TaskResult.go
@@ -12,9 +12,47 @@ type TaskResult struct {
 
 const (
 	STATUS_AC  = iota
-	STATUS_CE
+	_
 	STATUS_RE
 	STATUS_TLE
 	STATUS_MLE
 	STATUS_WA
 )
+
+func GetAccepptedTaskResult(runtime, memory int64) TaskResult {
+	return TaskResult{
+		Status:  STATUS_AC,
+		Runtime: runtime,
+		Memory:  memory,
+	}
+}
+
+func GetRuntimeErrorTaskResult() TaskResult {
+	return TaskResult{
+		Status: STATUS_RE,
+		Error:  "Runtime Error",
+	}
+}
+
+func GetTimeLimitExceededErrorTaskResult() TaskResult {
+	return TaskResult{
+		Status: STATUS_TLE,
+		Error:  "Runtime Error",
+	}
+}
+
+func GetMemoryLimitExceededErrorTaskResult() TaskResult {
+	return TaskResult{
+		Status: STATUS_MLE,
+		Error:  "Runtime Error",
+	}
+}
+
+func GetWrongAnswerTaskResult(input, output, expected string) TaskResult {
+	return TaskResult{
+		Status:   STATUS_WA,
+		Input:    input,
+		Output:   output,
+		Expected: expected,
+	}
+}
