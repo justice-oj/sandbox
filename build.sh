20de9df8d0a0fcbeeefc00faccac01b613255005
liuchao21 <liuchao21@58ganji.com>
2017-09-04 12:57:23 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm bin/*
 
-go build -o bin/c_compiler src/sandbox/c/compiler.go
-go build -o bin/cpp_compiler src/sandbox/cpp/compiler.go
+go build -o bin/c_compiler src/sandbox/c/Compiler.go
+go build -o bin/cpp_compiler src/sandbox/cpp/Compiler.go
 
-go build -o bin/c_container src/sandbox/c/container.go
+go build -o bin/c_container src/sandbox/c/Container.go
