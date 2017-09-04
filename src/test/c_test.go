aee79575451701cb090ffdffc325c7d129e97d6e
liuchao21 <liuchao21@58ganji.com>
2017-09-04 14:17:23 +0800

    add first c test case

--- src/test/c_test.go
+++ src/test/c_test.go
@@ -4,14 +4,44 @@ import (
 	"testing"
 	"os"
 	"os/exec"
+	"bytes"
+	"strings"
 )
 
-func Test_C_0_Accepted(t *testing.T) {
+func Test_C_Accepted(t *testing.T) {
 	absPath, _ := os.Getwd()
+	baseDir, projectDir := absPath + "/tmp", absPath + "/../.."
 
-	cpCmd := exec.Command("cp", absPath + "/../resources/c/0.in", absPath + "/tmp/0/Main.c")
-	cpCmd.Run()
+	os.MkdirAll(baseDir, os.ModePerm)
 
-	cmd := exec.Command(absPath + "/../../bin/c_compiler", "-basedir=" + absPath + "/tmp/0/")
-	cmd.Run()
+	cpCmd := exec.Command("cp", projectDir + "/src/resources/c/0.in", baseDir + "/Main.c")
+	cpErr := cpCmd.Run()
+	if cpErr != nil {
+		os.RemoveAll(baseDir + "/")
+		t.Error("copy source file failed")
+	}
+
+	var compilerStderr bytes.Buffer
+	compilerArgs := []string{"-compiler=/usr/bin/gcc", "-basedir=" + baseDir, "-timeout=10000"}
+	compilerCmd := exec.Command(projectDir + "/bin/c_compiler", compilerArgs...)
+	compilerCmd.Stderr = &compilerStderr
+	compilerCmd.Run()
+	if compilerStderr.Len() > 0 {
+		os.RemoveAll(baseDir + "/")
+		t.Error(compilerStderr.String())
+	}
+
+	var containerStdout bytes.Buffer
+	containerArgs := []string{"-basedir=" + baseDir, "-timeout=10000", "-input=10:10:23AM", "-expected=10:10:23"}
+	containerCmd := exec.Command(projectDir + "/bin/c_container", containerArgs...)
+	containerCmd.Stdout = &containerStdout
+	containerCmd.Run()
+
+	result := containerStdout.String()
+	if !strings.Contains(result, "\"status\":0") {
+		os.RemoveAll(baseDir + "/")
+		t.Error(result)
+	}
+
+	os.RemoveAll(baseDir + "/")
 }
