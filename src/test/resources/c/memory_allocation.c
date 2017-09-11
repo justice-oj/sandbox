99e8a1e897b35fa98023d693f3125283981e8fb5
liupangzi <thesedays@126.com>
2017-09-12 01:05:15 +0800

    add memory

--- src/test/resources/c/memory_allocation.c
+++ src/test/resources/c/memory_allocation.c
@@ -4,12 +4,11 @@
 #include <unistd.h>
 
 int main(void) {
-    int chunk_size = 1024 * 1024;
+    int chunk_size = 1024 * 1024 * 1024;
     void *p = NULL;
 
     while(1) {
         if ((p = malloc(chunk_size*sizeof(float))) == NULL) {
-            printf("Out of memory");
             break;
         }
         memset(p, 1, chunk_size);
