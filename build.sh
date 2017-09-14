da77cadbd64d813919186451070ab0789c5bf7eb
liuchao21 <liuchao21@58ganji.com>
2017-09-14 16:15:00 +0800

    update to unix newline

--- build.sh
+++ build.sh
@@ -2,8 +2,16 @@
 
 rm bin/*
 
+echo "Download go packages..."
+go get "github.com/getsentry/raven-go"
+go get "github.com/docker/docker/pkg/reexec"
+go get "github.com/satori/go.uuid"
+
+echo "Compile binaries..."
 go build -o bin/c_compiler src/sandbox/c/Compiler.go
 go build -o bin/cpp_compiler src/sandbox/cpp/Compiler.go
 
 go build -o bin/c_container src/sandbox/c/Container.go
 go build -o bin/cpp_container src/sandbox/cpp/Container.go
+
+echo "Done"
