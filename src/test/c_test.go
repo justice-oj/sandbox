760b383bd4fa83cd1a7b396f7d31c4f04da8d868
liuchao21 <liuchao21@58ganji.com>
2017-09-19 16:54:53 +0800

    refactor test methods

--- src/test/c_test.go
+++ src/test/c_test.go
@@ -17,16 +17,11 @@ func copyCSourceFile(name string, t *testing.T) (string, string) {
 	baseDir, projectDir := absPath+"/tmp", absPath+"/../.."
 	os.MkdirAll(baseDir, os.ModePerm)
 
-	cpCmd := exec.Command("cp", projectDir+"/src/test/resources/c/"+name, baseDir+"/Main.c")
-	cpErr := cpCmd.Run()
-
-	if cpErr != nil {
-		os.RemoveAll(baseDir + "/")
-		t.Error(cpErr.Error())
-		t.FailNow()
+	cmd := exec.Command("cp", projectDir+"/src/test/resources/c/"+name, baseDir+"/Main.c")
+	if err := cmd.Run(); err != nil {
+		t.Error(err)
 	}
 
-	t.Log("Done")
 	return baseDir, projectDir
 }
 
@@ -35,19 +30,14 @@ func copyCSourceFile(name string, t *testing.T) (string, string) {
 func compileC(name, baseDir, projectDir string, t *testing.T) (string) {
 	t.Logf("Compiling file %s ...", name)
 
-	var compilerStderr bytes.Buffer
-	compilerCmd := exec.Command(projectDir+"/bin/c_compiler", "-basedir="+baseDir)
-	compilerCmd.Stderr = &compilerStderr
-	compilerErr := compilerCmd.Run()
-
-	if compilerErr != nil {
-		os.RemoveAll(baseDir + "/")
-		t.Error(compilerErr.Error())
-		t.FailNow()
+	var stderr bytes.Buffer
+	cmd := exec.Command(projectDir+"/bin/c_compiler", "-basedir="+baseDir)
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		t.Error(err)
 	}
 
-	t.Log("Done")
-	return compilerStderr.String()
+	return stderr.String()
 }
 
 // HELPER
@@ -55,292 +45,232 @@ func compileC(name, baseDir, projectDir string, t *testing.T) (string) {
 func runC(baseDir, projectDir, memory, timeout string, t *testing.T) (string) {
 	t.Log("Running binary /Main ...")
 
-	var containerStdout, containerStderr bytes.Buffer
-	containerArgs := []string{"-basedir=" + baseDir, "-input=10:10:23PM", "-expected=22:10:23", "-memory=" + memory, "-timeout=" + timeout}
-	containerCmd := exec.Command(projectDir+"/bin/c_container", containerArgs...)
-	containerCmd.Stdout = &containerStdout
-	containerCmd.Stderr = &containerStderr
-
-	if err := containerCmd.Run(); err != nil {
-		os.RemoveAll(baseDir + "/")
-		t.Error(err.Error())
-		t.FailNow()
+	var stdout, stderr bytes.Buffer
+	args := []string{"-basedir=" + baseDir, "-input=10:10:23PM", "-expected=22:10:23", "-memory=" + memory, "-timeout=" + timeout}
+	cmd := exec.Command(projectDir+"/bin/c_container", args...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		t.Error(err)
 	}
 
-	t.Log(containerStderr.String())
-	return containerStdout.String()
+	t.Log(stderr.String())
+	return stdout.String()
 }
 
 func Test_C_AC(t *testing.T) {
 	name := "ac.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
 
-	containerErr := runC(baseDir, projectDir, "64", "1000", t)
-	if !strings.Contains(containerErr, "\"status\":0") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr + " => status != 0")
-		t.FailNow()
+	containerOutput := runC(baseDir, projectDir, "64", "1000", t)
+	if !strings.Contains(containerOutput, "\"status\":0") {
+		t.Error(containerOutput + " => status != 0")
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Compiler_Bomb_0(t *testing.T) {
 	name := "compiler_bomb_0.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if !strings.Contains(compilerStderr, "signal: killed") {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr + " => Compile error does not contain string `signal: killed`")
-		t.FailNow()
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Compiler_Bomb_1(t *testing.T) {
 	name := "compiler_bomb_1.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if !strings.Contains(compilerStderr, "signal: killed") {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr + " => Compile error does not contain string `signal: killed`")
-		t.FailNow()
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Compiler_Bomb_2(t *testing.T) {
 	name := "compiler_bomb_2.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if !strings.Contains(compilerStderr, "signal: killed") {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr + " => Compile error does not contain string `signal: killed`")
-		t.FailNow()
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Fork_Bomb(t *testing.T) {
 	name := "fork_bomb.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
 
-	containerErr := runC(baseDir, projectDir, "64", "1000", t)
-
-	if !strings.Contains(containerErr, "Runtime Error") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr)
-		t.FailNow()
+	// got `signal: killed`
+	containerOutput := runC(baseDir, projectDir, "64", "1000", t)
+	if !strings.Contains(containerOutput, "Runtime Error") {
+		t.Error(containerOutput)
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Get_Host_By_Name(t *testing.T) {
 	name := "get_host_by_name.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
-
-	containerErr := runC(baseDir, projectDir, "64", "1000", t)
 
 	// Main.c:(.text+0x28): warning: Using 'gethostbyname' in statically linked applications
 	// requires at runtime the shared libraries from the glibc version used for linking
-	if !strings.Contains(containerErr, "\"status\":2") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr)
-		t.FailNow()
+	// got `exit status 1`
+	containerOutput := runC(baseDir, projectDir, "64", "1000", t)
+	if !strings.Contains(containerOutput, "\"status\":2") {
+		t.Error(containerOutput)
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Include_Leaks(t *testing.T) {
 	name := "include_leaks.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if !strings.Contains(compilerStderr, "/etc/shadow") {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr + " => Compile error does not contain string `/etc/shadow`")
-		t.FailNow()
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Infinite_Loop(t *testing.T) {
 	name := "infinite_loop.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
 
-	containerErr := runC(baseDir, projectDir, "64", "1000", t)
-
-	if !strings.Contains(containerErr, "Runtime Error") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr)
-		t.FailNow()
+	// got `signal: killed`
+	containerOutput := runC(baseDir, projectDir, "64", "1000", t)
+	if !strings.Contains(containerOutput, "Runtime Error") {
+		t.Error(containerOutput)
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Memory_Allocation(t *testing.T) {
 	name := "memory_allocation.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
-
-	containerErr := runC(baseDir, projectDir, "8", "5000", t)
 
 	// `Killed` is sent to tty by kernel (and record will also be kept in /var/log/message)
 	// both stdout and stderr are empty which will lead to status WA
-	// OR
+	// OR...
 	// just running out of time
-	if !strings.ContainsAny(containerErr, "\"status\":5 & Runtime Error") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr)
-		t.FailNow()
+	containerOutput := runC(baseDir, projectDir, "8", "5000", t)
+	if !strings.ContainsAny(containerOutput, "\"status\":5 & Runtime Error") {
+		t.Error(containerOutput)
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Plain_Text(t *testing.T) {
 	name := "plain_text.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
 
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if !strings.Contains(compilerStderr, "error") {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr + " => Compile error does not contain string `error`")
-		t.FailNow()
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Run_Command_Line_0(t *testing.T) {
 	name := "run_command_line_0.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
 
-	containerErr := runC(baseDir, projectDir, "16", "1000", t)
-
-	if !strings.Contains(containerErr, "\"status\":5") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr)
-		t.FailNow()
+	containerOutput := runC(baseDir, projectDir, "16", "1000", t)
+	if !strings.Contains(containerOutput, "\"status\":5") {
+		t.Error(containerOutput)
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Run_Command_Line_1(t *testing.T) {
 	name := "run_command_line_1.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
 
-	containerErr := runC(baseDir, projectDir, "16", "1000", t)
-
-	if !strings.Contains(containerErr, "\"status\":5") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr)
-		t.FailNow()
+	containerOutput := runC(baseDir, projectDir, "16", "1000", t)
+	if !strings.Contains(containerOutput, "\"status\":5") {
+		t.Error(containerOutput)
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_Syscall_0(t *testing.T) {
 	name := "syscall_0.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
 
-	containerErr := runC(baseDir, projectDir, "16", "1000", t)
-
-	if !strings.Contains(containerErr, "\"status\":5") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr)
-		t.FailNow()
+	containerOutput := runC(baseDir, projectDir, "16", "1000", t)
+	if !strings.Contains(containerOutput, "\"status\":5") {
+		t.Error(containerOutput)
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
 
 func Test_C_TCP_Client(t *testing.T) {
 	name := "tcp_client.c"
 	baseDir, projectDir := copyCSourceFile(name, t)
+	defer os.RemoveAll(baseDir)
+
 	compilerStderr := compileC(name, baseDir, projectDir, t)
-
 	if len(compilerStderr) > 0 {
-		os.RemoveAll(baseDir + "/")
 		t.Error(compilerStderr)
-		t.FailNow()
+		return
 	}
 
-	containerErr := runC(baseDir, projectDir, "16", "5000", t)
-
-	if !strings.Contains(containerErr, "Runtime Error") {
-		os.RemoveAll(baseDir + "/")
-		t.Error(containerErr)
-		t.FailNow()
+	containerOutput := runC(baseDir, projectDir, "16", "5000", t)
+	if !strings.Contains(containerOutput, "Runtime Error") {
+		t.Error(containerOutput)
 	}
-
-	os.RemoveAll(baseDir + "/")
 }
