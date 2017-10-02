9535b5eb69787c6fcd87773114ef56779f3c35b8
liupangzi <thesedays@126.com>
2017-10-02 12:37:51 +0800

    add cgroup auto cleaning-up to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm bin/c_compiler bin/c_container bin/cpp_compiler bin/cpp_container
-
 echo "Download go packages..."
 go get "github.com/getsentry/raven-go"
 go get "github.com/docker/docker/pkg/reexec"
@@ -13,5 +11,14 @@ go build -o bin/cpp_compiler src/cmd/cpp/compiler.go
 
 go build -o bin/c_container src/cmd/c/container.go
 go build -o bin/cpp_container src/cmd/cpp/container.go
+
+echo "Enable automatically removing empty cgroups..."
+echo 1 > /sys/fs/cgroup/cpu/notify_on_release
+echo 1 > /sys/fs/cgroup/memory/notify_on_release
+echo 1 > /sys/fs/cgroup/pids/notify_on_release
+
+echo "/opt/justice-sandbox/bin/clean_cpu_cgroup.sh" > /sys/fs/cgroup/cpu/release_agent
+echo "/opt/justice-sandbox/bin/clean_memory_cgroup.sh" > /sys/fs/cgroup/memory/release_agent
+echo "/opt/justice-sandbox/bin/clean_pids_cgroup.sh" > /sys/fs/cgroup/pids/release_agent
 
 echo "Done"
