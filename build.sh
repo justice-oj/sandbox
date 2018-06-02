9b391f4bffae1ee5eecb382f7d91e37c437b3f91
liupangzi <thesedays@126.com>
2018-06-02 12:02:14 +0000

    add godeps support

--- build.sh
+++ build.sh
@@ -2,15 +2,11 @@
 
 echo -e "[DEBUG] GOPATH is \e[34m\e[1m${GOPATH}\033[0m"
 
-echo "Download go packages..."
-go get "github.com/getsentry/raven-go"
-go get "github.com/docker/docker/pkg/reexec"
-go get "github.com/satori/go.uuid"
-
 echo "Compile binaries..."
 mkdir -p "/opt/justice-sandbox/bin"
-go build -o /opt/justice-sandbox/bin/clike_compiler ${GOPATH}/src/github.com/justice-oj/sandbox/src/cmd/clike/compiler.go
-go build -o /opt/justice-sandbox/bin/clike_container ${GOPATH}/src/github.com/justice-oj/sandbox/src/cmd/clike/container.go
+cd ${GOPATH}/src/github.com/justice-oj/sandbox/src/
+go build -o /opt/justice-sandbox/bin/clike_compiler compiler.go
+go build -o /opt/justice-sandbox/bin/clike_container container.go
 
 echo "Enable automatically removing empty cgroups..."
 echo 1 > /sys/fs/cgroup/cpu/notify_on_release
