e785ca92da33450a5b376825d14241976efc3de7
刘超 <liuchao.rd@bytedance.com>
2019-07-13 23:10:58 +0800

    :candy: use go mod

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo -e "[DEBUG] GOPATH is \e[34m\e[1m${GOPATH}\033[0m"
-
 echo "Compile binaries..."
 mkdir -p "/opt/justice-sandbox/bin"
-cd ${GOPATH}/src/github.com/justice-oj/sandbox/
 go build -o /opt/justice-sandbox/bin/clike_compiler compiler.go
 go build -o /opt/justice-sandbox/bin/clike_container container.go
 
@@ -13,8 +10,8 @@ echo 1 > /sys/fs/cgroup/cpu/notify_on_release
 echo 1 > /sys/fs/cgroup/memory/notify_on_release
 echo 1 > /sys/fs/cgroup/pids/notify_on_release
 
-echo "${GOPATH}/src/github.com/justice-oj/sandbox/scripts/clean_cpu_cgroup.sh" > /sys/fs/cgroup/cpu/release_agent
-echo "${GOPATH}/src/github.com/justice-oj/sandbox/scripts/clean_memory_cgroup.sh" > /sys/fs/cgroup/memory/release_agent
-echo "${GOPATH}/src/github.com/justice-oj/sandbox/scripts/clean_pids_cgroup.sh" > /sys/fs/cgroup/pids/release_agent
+echo "${PWD}/scripts/clean_cpu_cgroup.sh" > /sys/fs/cgroup/cpu/release_agent
+echo "${PWD}/scripts/clean_memory_cgroup.sh" > /sys/fs/cgroup/memory/release_agent
+echo "${PWD}/scripts/clean_pids_cgroup.sh" > /sys/fs/cgroup/pids/release_agent
 
 echo "Done!"
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/justice-oj/sandbox
+
+go 1.12
+
+require (
+	github.com/docker/docker v1.13.1
+	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c
+	github.com/jtolds/gls v4.20.0+incompatible
+	github.com/satori/go.uuid v1.2.0
+	github.com/smartystreets/assertions v0.0.0-20190401211740-f487f9de1cd3
+	github.com/smartystreets/goconvey v0.0.0-20170602164621-9e8dc3f972df
+)
