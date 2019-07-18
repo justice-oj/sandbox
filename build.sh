bdc2bc5dbdd2fc5955b0ef5fa80be01defbe1613
liupangzi <thesedays@126.com>
2019-07-18 03:53:18 +0000

    use go mod in build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export GO111MODULE=on
+
 echo "Compile binaries..."
-mkdir -p "/opt/justice-sandbox/bin"
-go build -o /opt/justice-sandbox/bin/clike_compiler compiler.go
-go build -o /opt/justice-sandbox/bin/clike_container container.go
+mkdir -p "${PWD}/bin"
+go build -o ${PWD}/bin/clike_compiler compiler.go
+go build -o ${PWD}/bin/clike_container container.go
 
 echo "Enable automatically removing empty cgroups..."
 echo 1 > /sys/fs/cgroup/cpu/notify_on_release
